441d5ed594403e13dc1bc3c8d04785ee4a75f37e
Neschetnyi <a.neschetnyi@gmail.com>
2025-06-24 17:43:05 +0500

    2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let button = document.querySelector(".startButton");
 let ok = document.querySelector(".ok");
 let errorOut = document.querySelector(".error");
 let countOut = document.querySelector(".tryCount");
+let strartId = document.querySelector(".strartId");
 
 async function fetchLastId() {
   try {
@@ -13,6 +14,14 @@ async function fetchLastId() {
     console.log(responce);
     const html = await responce.text();
     console.log(html);
+    if (
+      html.includes("We tried looking everywhere, and we couldn't find it.") ||
+      html.includes("This page cannot be found.")
+    ) {
+      errorOut.textContent = `ID ${id} не найден`;
+    } else {
+      ok.textContent = `ID ${id} найден!`;
+    }
   } catch (error) {
     console.log(error);
   }
