9b79b34c1a6ddb5d144a8a83d5dd61cb0f0af4fb
Neschetnyi <a.neschetnyi@gmail.com>
2025-06-24 18:16:21 +0500

    5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var id = 7000000;
+var count = 0;
 
 let button = document.querySelector(".startButton");
 let ok = document.querySelector(".ok");
@@ -13,19 +14,25 @@ startId.addEventListener("input", () => {
 
 async function fetchLastId() {
   try {
-    const responce = await fetch(
+    const response = await fetch(
       `https://serverforfetching.onrender.com/beer/${id}`
     );
-    console.log(responce);
-    const html = await responce.text();
-    console.log(html);
-    if (
+    const html = await response.text();
+
+    const notFound =
       html.includes("We tried looking everywhere, and we couldn't find it.") ||
-      html.includes("This page cannot be found.")
-    ) {
+      html.includes("This page cannot be found.");
+
+    if (notFound) {
       errorOut.textContent = `ID ${id} не найден`;
+      ok.textContent = "";
+      count++;
+      countOut.textContent = count;
+      id--;
+      await fetchLastId(); // повторяем
     } else {
       ok.textContent = `ID ${id} найден!`;
+      errorOut.textContent = "";
     }
   } catch (error) {
     console.log(error);
